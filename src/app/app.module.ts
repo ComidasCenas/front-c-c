253f023bf5ef5bf5b37fc830bc1daf63b7625d70
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-12-09 13:53:50 +0100

    Add ngrx configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; 
+import { EffectsModule } from '@ngrx/effects'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomRouterSerializer } from './custom-router-serializer';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ router: routerReducer }),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
+    StoreRouterConnectingModule.forRoot({ serializer: CustomRouterSerializer })
   ],
   providers: [],
   bootstrap: [AppComponent]
