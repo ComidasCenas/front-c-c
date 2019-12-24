24bf84e8e72cdfc2d1764fe1ace2a4882e03a595
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-12-24 17:46:54 +0100

    Add animation for open and close menu
    - Add ngIf in the menu component
    - Add animation trigger
    - Add styles for cursor and focus
    - Add animation definition

--- src/components/drop-down-menu/drop-down-menu.component.ts
+++ src/components/drop-down-menu/drop-down-menu.component.ts
@@ -1,10 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MenuConfig } from './drop-down-menu.entities';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'cc-drop-down-menu',
   templateUrl: './drop-down-menu.component.html',
-  styleUrls: ['./drop-down-menu.component.scss']
+  styleUrls: ['./drop-down-menu.component.scss'],
+  animations: [
+    trigger('openCloseMenu', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-out', style({ opacity: 1}))
+      ]),
+      transition(':leave',[
+        style({ opacity: 1}),
+        animate('0.3s ease-in', style({ opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class DropDownMenuComponent implements OnInit {
 
@@ -20,6 +33,7 @@ export class DropDownMenuComponent implements OnInit {
   public menuPosY = 0;
 
   public menuHeight = 0;
+  public menuOpen = false;
 
   ngOnInit() {
     const numOptions = this.config.options.length;
